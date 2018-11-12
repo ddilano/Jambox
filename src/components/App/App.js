75575798c3c13ed88890f5ee1512a67a7b67ed77
Dilan Ozusta <dilanozusta@outlook.com>
2018-11-11 22:24:55 -0600

    changed login link redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,7 +28,7 @@ class App extends Component {
   render() {
 
     let returnVal = (
-    <a href="https://jambox-party.herokuapp.com/login">
+    <a href="https://jambox-server.herokuapp.com">
       <button>Login with spotify</button>
     </a>);
     if (this.props.isLoggedIn){
